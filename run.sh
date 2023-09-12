d6eb47c2f85b5da2b3aef6f350e4f1c5eff2eedb
David Zhang <zdavid0123@gmail.com>
2023-09-12 13:39:57 -0400

    add demo script

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+indivs=020
+voxels=128
+noise=gauss
+seed=00
+datafile="data.pickle"
+outpref="results/"
+
+python generate_data_synthetic.py \
+    --n-voxels=${voxels} \
+    --n-indivs=${indivs} \
+    --beta-stn=0.10 \
+    --noise-dist=${noise} \
+    --seed=${seed} \
+    --out=${datafile}
+
+python run_nnisr.py $datafile $outpref
